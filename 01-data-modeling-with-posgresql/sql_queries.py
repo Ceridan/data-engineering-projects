bfd0894c19c36e130ffe0a5ee61eab6716bfd16f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2019-12-16 22:28:12 +0300

    feat: Add constraints and ON CONFLICT policies to tables

--- 01-data-modeling-with-posgresql/sql_queries.py
+++ 01-data-modeling-with-posgresql/sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""
     CREATE TABLE songplays(
-        songplay_id bigserial,
+        songplay_id bigserial PRIMARY KEY,
         start_time timestamp,
         user_id int,
         level varchar(100),
@@ -24,18 +24,18 @@ songplay_table_create = ("""
 
 user_table_create = ("""
     CREATE TABLE users(
-        user_id int NOT NULL,
+        user_id int PRIMARY KEY,
         first_name varchar(100),
         last_name varchar(100),
         gender char(1),
-        level varchar(100)
+        level varchar(100) NOT NULL
     );
 """)
 
 song_table_create = ("""
     CREATE TABLE songs(
-        song_id char(18) NOT NULL,
-        title varchar(100),
+        song_id char(18) PRIMARY KEY,
+        title varchar(100) NOT NULL,
         artist_id char(18),
         year int,
         duration numeric
@@ -44,8 +44,8 @@ song_table_create = ("""
 
 artist_table_create = ("""
     CREATE TABLE artists(
-        artist_id char(18) NOT NULL,
-        name varchar(100),
+        artist_id char(18) PRIMARY KEY,
+        name varchar(100) NOT NULL,
         location text,
         latitude numeric,
         longitude numeric
@@ -54,7 +54,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     CREATE TABLE time(
-        start_time timestamp NOT NULL,
+        start_time timestamp PRIMARY KEY,
         hour int,
         day int,
         week int,
@@ -71,24 +71,46 @@ songplay_table_insert = ("""
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
+# In case of several attempts to insert data to the users table, they should be considered as an update operation.
+# The user can change name and even gender, and what is most important for our business - he or she can switch from
+# free level to paid level and vice versa.
 user_table_insert = ("""
     INSERT INTO users(user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE
+    SET first_name = excluded.first_name
+        , last_name = excluded.last_name
+        , gender = excluded.gender
+        , level = excluded.level;
 """)
 
+# The information about song maybe incomplete on the first insert. We want to update missing fields
+# if we could find information about current  song from more sources.
 song_table_insert = ("""
     INSERT INTO songs(song_id, title, artist_id, year, duration)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+     ON CONFLICT (song_id) DO UPDATE
+     SET artist_id = COALESCE(excluded.artist_id, songs.artist_id)
+        , year = COALESCE(excluded.year, songs.year)
+        , duration = COALESCE(excluded.duration, songs.duration);
 """)
 
+# Same as with songs we want to update missing information
 artist_table_insert = ("""
     INSERT INTO artists(artist_id, name, location, latitude, longitude)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO UPDATE
+    SET location = COALESCE(excluded.location, artists.location)
+        , latitude = COALESCE(excluded.latitude, artists.latitude)
+        , longitude = COALESCE(excluded.longitude, artists.longitude);
 """)
 
+# We do not want to update time table, because the infromation will be the same for same start_time.
+# Even if we lost some data in non-index fields we will easier recover it with single update using PRIMARY KEY data.
 time_table_insert = ("""
     INSERT INTO time(start_time, hour, day, week, month, year, weekday) 
-    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
